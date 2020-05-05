a59f4394d95e8ce18aec6e162a90d899add5debf
luis705 <luisotaviolamorim@gmail.com>
2020-05-05 13:09:59 -0300

    Added new python file in order to separate auxiliary functions of the game file

--- game.py
+++ game.py
@@ -1,58 +1,10 @@
 import time
+from auxiliary_functions import *
+
 
 table = [[' ' for i in range(3)] for j in range(3)]
 points_table = [[0 for i in range(3)] for j in range(3)]
 curr_player = 1
-
-
-def menu():
-    #  Function to print the game menu
-
-    print('/-------------------\ \n'
-          '|                   |\n'
-          '|     MAIN MENU     |\n'
-          '|                   |\n'
-          '|  1 - New Game     |\n'
-          '|  2 - Instructions |\n'
-          '|  3 - Close        |\n'
-          '|                   |\n'
-          '\-------------------/')
-
-    #  Choice validation
-    while True:
-        choice = input()
-
-        if choice != '1' and choice != '2' and choice != '3':
-            print('You must type 1, 2 or 3')
-        else:
-            return choice
-
-
-def show_table(table):
-
-    print(f' {table[0][0]} | {table[0][1]} | {table[0][2]} \n'
-          '---|---|---\n'
-          f' {table[1][0]} | {table[1][1]} | {table[1][2]} \n'
-          '---|---|---\n'
-          f' {table[2][0]} | {table[2][1]} | {table[2][2]} \n')
-
-
-def game_modes():
-    print('/--------------------\ \n'
-          '|                    |\n'
-          '|  Choose game mode  |\n'
-          '|   1 - 1 player     |\n'
-          '|   2 - 2 players    |\n'
-          '|                    |\n'
-          '\--------------------/')
-
-    while True:
-        mode = input()
-
-        if mode != '1' and mode != '2':
-            print('You must choose between 1 and 2')
-        else:
-            return mode
 
 
 def play(player, table, points_table):
@@ -202,7 +154,7 @@ def game(computer):
     global table
     global points_table
 
-    while True:
+    while computer == 2:
         print(f"Player's {curr_player} turn, where do you want to put your piece?")
         show_table(table)
         play(curr_player, table, points_table)
@@ -223,32 +175,6 @@ def game(computer):
             curr_player = 1
             break
         time.sleep(.5)
-
-
-def is_finished(table):
-    if 0 in table[0] or 0 in table[1] or 0 in table[2]:
-        return False
-    return True
-
-
-def winner(table):
-
-    if sum(table[0]) == 3 or sum(table[1]) == 3 or sum(table[2]) == 3\
-            or table[0][0] + table[1][0] + table[2][0] == 3\
-            or table[0][1] + table[1][1] + table[2][1] == 3\
-            or table[0][2] + table[1][2] + table[2][2] == 3\
-            or table[0][0] + table[1][1] + table[2][2] == 3\
-            or table[0][2] + table[1][1] + table[2][0] == 3:
-        return 1
-    elif sum(table[0]) == 6 or sum(table[1]) == 6 or sum(table[2]) == -3\
-            or table[0][0] + table[1][0] + table[2][0] == -3\
-            or table[0][1] + table[1][1] + table[2][1] == -3\
-            or table[0][2] + table[1][2] + table[2][2] == -3\
-            or table[0][0] + table[1][1] + table[2][2] == -3\
-            or table[0][2] + table[1][1] + table[2][0] == -3:
-        return 2
-    else:
-        return 0
 
 
 while True:
